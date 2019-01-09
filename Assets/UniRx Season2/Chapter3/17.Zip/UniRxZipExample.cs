6a308733bab56055f8d7514f84dead94f3bb05d7
John_Q <qiucheng93@163.com>
2019-01-09 23:11:48 +0800

    update 0109

---
+++ Assets/UniRx Season2/Chapter3/17.Zip/UniRxZipExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class UniRxZipExample : MonoBehaviour {
+
+	void Start () {
+        var leftStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0)).Select(__ => "left");
+        var rightStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(1)).Select(__ => "right");
+
+        leftStream.Zip(rightStream, (left, right) => left + " " + right)
+                  .Subscribe(content =>
+                  {
+                      Debug.Log(content);
+                  });
+	}
+}
