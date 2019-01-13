3e61fbc7a2358d87fa4bde4973544e371083c050
John_Q <john.qc93@gmail.com>
2019-01-13 14:20:27 +0800

    update 01/13

---
+++ Assets/UniRx Season2/Chapter4/12.TimeInterval/UniRxTimeIntervalExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class UniRxTimeIntervalExample : MonoBehaviour {
+
+	void Start () {
+        Observable.EveryUpdate()
+                  .Where(_ => Input.GetMouseButtonDown(0))
+                  .Select(__ => "clicked")
+                  .TimeInterval()
+                  .Subscribe(timeInterval => Debug.LogFormat("{0},{1}", timeInterval.Interval, timeInterval.Value));
+	}
+}
