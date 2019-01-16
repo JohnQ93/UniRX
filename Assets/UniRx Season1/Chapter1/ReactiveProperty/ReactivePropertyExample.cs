509b76cd6bafda200f9537dfc6580dae47504e4a
John_Q <qiucheng93@163.com>
2019-01-16 23:50:05 +0800

    update 1/16

--- Assets/UniRx Season1/Chapter1/ReactiveProperty/ReactivePropertyExample.cs
+++ Assets/UniRx Season1/Chapter1/ReactiveProperty/ReactivePropertyExample.cs
@@ -7,18 +7,37 @@ using UniRx;
 public class ReactivePropertyExample : MonoBehaviour {
 
 	public ReactiveProperty<int> Age = new ReactiveProperty<int>();
-	// Use this for initialization
-	void Start () {
-		Age.Subscribe(age =>
-		{
-			Debug.Log("age changed");
-		});
-		Age.Value = 10;
-	}
+    private int kinds = 0;
+    private IDisposable disposable;
 
-	// Update is called once per frame
-	void Update () {
+	void OnEnable () {
+        //Age.Subscribe(age =>
+        //{
+        //	Debug.Log("age changed");
+        //});
+        //Age.Value = 10;
 
-	}
+        disposable = Age.Where(age => age > 0)
+           .First()
+           .Subscribe(_ =>
+           {
+               kinds++;
+               Debug.LogFormat("kinds == {0}", kinds);
+           }).AddTo(this);
+        
+
+        Observable.Interval(TimeSpan.FromSeconds(2.0f))
+                  .Subscribe(_ =>
+                  {
+                      Age.Value++;
+                  });
+    }
+
+    private void OnDisable()
+    {
+        disposable.Dispose();
+        Age.Value = 0;
+        kinds = 0;
+    }
 }
 
